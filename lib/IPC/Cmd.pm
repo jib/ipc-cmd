6c3e7c87e977cfc42bc2f3cc4afd677fd0f3b807
Jos Boumans <kane@cpan.org>
2006-11-24 13:58:33 +0000

    r3619@rimmer:  kane | 2006-11-24 14:57:28 +0100  * this be 0.36
    git-svn-id: http://oss.dwim.org/ipc-cmd@1681 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/IPC/Cmd.pm
+++ lib/IPC/Cmd.pm
@@ -13,7 +13,7 @@ BEGIN {
                         $USE_IPC_RUN $USE_IPC_OPEN3 $WARN
                     ];
 
-    $VERSION        = '0.35_01';
+    $VERSION        = '0.36';
     $VERBOSE        = 0;
     $DEBUG          = 0;
     $WARN           = 1;
