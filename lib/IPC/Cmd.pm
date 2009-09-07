65f42634eab817e9a9c0916956afd0137866216b
Chris Williams <chris@bingosnet.co.uk>
2009-09-07 14:14:07 +0000

    This be 0.48
    git-svn-id: http://oss.dwim.org/ipc-cmd@2660 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/IPC/Cmd.pm
+++ lib/IPC/Cmd.pm
@@ -16,7 +16,7 @@ BEGIN {
                         $USE_IPC_RUN $USE_IPC_OPEN3 $WARN
                     ];
 
-    $VERSION        = '0.46';
+    $VERSION        = '0.48';
     $VERBOSE        = 0;
     $DEBUG          = 0;
     $WARN           = 1;
