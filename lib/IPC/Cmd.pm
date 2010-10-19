8fb4603f4dc3f23cc9409e64f8faa2109d6d4e94
Chris Williams <chris@bingosnet.co.uk>
2010-10-19 18:02:38 +0000

    Bump version and update CHANGES
    git-svn-id: http://oss.dwim.org/ipc-cmd@2832 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/IPC/Cmd.pm
+++ lib/IPC/Cmd.pm
@@ -17,7 +17,7 @@ BEGIN {
                         $INSTANCES
                     ];
 
-    $VERSION        = '0.62';
+    $VERSION        = '0.64';
     $VERBOSE        = 0;
     $DEBUG          = 0;
     $WARN           = 1;
