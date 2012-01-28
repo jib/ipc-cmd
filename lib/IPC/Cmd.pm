2772b1f603f5b87c5f8c3c5b12069a705fa58fda
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-01-28 16:14:37 +0000

    Bump to version 0.74 and update CHANGES

--- lib/IPC/Cmd.pm
+++ lib/IPC/Cmd.pm
@@ -17,7 +17,7 @@ BEGIN {
                         $INSTANCES
                     ];
 
-    $VERSION        = '0.72';
+    $VERSION        = '0.74';
     $VERBOSE        = 0;
     $DEBUG          = 0;
     $WARN           = 1;
