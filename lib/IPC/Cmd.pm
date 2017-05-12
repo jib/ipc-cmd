d546085cf6572eb38e5e46e9ea9a085d2050679c
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2017-05-12 17:02:43 +0100

    Release engineering for 0.98

--- lib/IPC/Cmd.pm
+++ lib/IPC/Cmd.pm
@@ -18,7 +18,7 @@ BEGIN {
                         $HAVE_MONOTONIC
                     ];
 
-    $VERSION        = '0.96';
+    $VERSION        = '0.98';
     $VERBOSE        = 0;
     $DEBUG          = 0;
     $WARN           = 1;
